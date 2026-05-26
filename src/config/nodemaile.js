9855d20e96499b7322f692a06abf19d70b018228
josue <josue.patino@epn.edu.ec>
2026-05-26 00:04:37 -0500

    se cambia swagger

--- src/config/nodemaile.js
+++ src/config/nodemaile.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    secure: false,
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.USER_MAILTRAP,
         pass: process.env.PASS_MAILTRAP,
